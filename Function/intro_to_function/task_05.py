1cb7997e770c4f1f856f8939b1f384fbd8dedfa7
porte <70mr.mystory@gmail.com>
2023-05-18 19:51:41 +0300

    intro to function.Finished task_05

--- Function/intro_to_function/task_05.py
+++ Function/intro_to_function/task_05.py
@@ -1,8 +1,19 @@
-def speed_to_pace(speed):
-    sec = int(3600 / speed)
-    minute = int(sec // 60)
-    sec = sec - minute * 60
-    return f"{minute}:{sec}"
+import math
 
-t = speed_to_pace(12)
-print(t)
+
+def speed_to_pace(speed):
+    total_sec = 3600 / speed
+    minutes = int(total_sec / 60)
+    sec = total_sec % 60
+    return "{}:{:02.0f}".format(minutes, sec)
+
+print(speed_to_pace(12.5))
+#---------------------------------------------------
+def speed_to_pace(speed):
+    """
+    Функция переводит скорость (speed) в км/ч в темп (км/мин).
+    """
+    temp = 60 / speed
+    minutes = int(temp)
+    seconds = (temp - minutes) * 60
+    return "{}:{:02.0f}".format(minutes, seconds)
