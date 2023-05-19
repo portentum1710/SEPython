2d2474230d0c2f42ec41000bca6377fdd667d322
Katya <Katya11091@outlook.com>
2023-05-19 04:20:22 +0300

    Solved task_01

--- Function/intro_to_function_2/task_01.py
+++ Function/intro_to_function_2/task_01.py
@@ -1,7 +1,13 @@
 import sys
 
 # Напишите вашу функцию тут.
-
+def sum_mult_index(lst):
+    sum = 0
+    for indx, num in enumerate(lst):
+        sum += indx * num
+    return sum
 # Не меняйте код ниже, он нужен для проверки задания
 values = list(map(int, sys.argv[1:]))
 print(sum_mult_index(values))
+
+
