623f7845b97799ad9569e1d1f970889060f7fbd1
porte <70mr.mystory@gmail.com>
2023-05-20 14:08:45 +0300

    Intro_to_function_3.Solved task_02

--- Function/intro_to_ function_3/task_02.py
+++ Function/intro_to_ function_3/task_02.py
@@ -1,15 +1,42 @@
-file_card = open("db.txt", "w+", encoding="utf-8")
-file_card.write("""123456;841
-855814;123
-471856;731""")
-file_card.close()
+# file_card = open("db.txt", "w+", encoding="utf-8")
+# file_card.write("""123456;841
+# 855814;123
+# 471856;731""")
+# file_card.close()
 
-card_number = "471856"
-floor = "733"
-for data in open("db.txt", "r", encoding="utf-8"):
-    if card_number in data and floor in data:
-        print("True")
-    if card_number in data and floor not in data:
-        print("False")
-    else:
-        print(None)
+
+# Создайте функцию check_access в этом месте
+
+def check_access(card, floor):
+    access = None
+    for data in open("db.txt", "r", encoding="utf-8"):
+        data = data.strip().split(";")
+        if card not in data[0]:
+            continue
+        if floor in data[1][0]:
+            access = "True"
+        else:
+            access = "False"
+            break
+    return access
+
+print(check_access("018471", "1"))
+#------------------------------------------------------------
+# Создайте функцию check_access в этом месте
+def check_access(card_number, floor):
+    for card in open("db.txt", "r"):
+        file_card_number, room = card.strip().split(";")
+        file_floor = room[0]
+        if card_number == file_card_number:
+            return floor == file_floor
+    return None
+
+
+# Не удаляйте код ниже, он нужен для тестирования вашей функции.
+import sys
+
+# Проверяем доступ
+access = check_access(sys.argv[1], sys.argv[2])
+
+# Получаем ответ для проверки
+print(access)
