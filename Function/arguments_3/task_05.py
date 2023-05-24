c49cebc72d7f638a4e6a47e2a78954d0046e96f5
porte <70mr.mystory@gmail.com>
2023-05-24 04:29:05 +0300

    The task_05 have been solved

--- Function/arguments_3/task_05.py
+++ Function/arguments_3/task_05.py
@@ -1,6 +1,15 @@
 def get_dict(text):
-    get_list = text.split(";")
-    return get_list
+    new_list = []
+    for item in text.split(";"):
+        new_list.append(item.split(":"))
+    return dict(new_list)
 
 result = get_dict("a:10;b:20;c:30")
 print(result)
+#-----------------------------------------
+# def get_dict(text: str):
+#     data = {}
+#     for item in text.split(";"):
+#         k, v = item.split(":")
+#         data[k] = v
+#     return data
