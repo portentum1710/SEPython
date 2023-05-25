f68944ad7d07f8930154c46bb1c485a007d04531
Katya <Katya11091@outlook.com>
2023-05-25 22:28:33 +0300

    add comments teacher

--- Function/arguments_3/task_06.py
+++ Function/arguments_3/task_06.py
@@ -6,8 +6,8 @@ def replace_all(text, num_dict):
 result = replace_all('My favorite animal is {animal}. My favorite color is {color}.', {"{animal}":"dog", "{color}":"blue"})
 print(result)
 #=====================================================
-# def replace_all(text: str, data: dict):
-#     for k, v in data.items():
-#         text = text.replace(k, v)
-#     return text
+def replace_all(text: str, data: dict):
+    for k, v in data.items():
+        text = text.replace(k, v)
+    return text
 
