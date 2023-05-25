e7b02316a09fa21f3726fce457eb10432a8c9ee3
Katya <Katya11091@outlook.com>
2023-05-25 22:26:52 +0300

    solved task_06 arguments_3

--- Function/arguments_3/task_06.py
+++ Function/arguments_3/task_06.py
@@ -1,12 +1,13 @@
 def replace_all(text, num_dict):
-    modify_text = ""
     for key in num_dict:
-        if key in text:
-            modify_text = text.replace(key, num_dict[key])
-    return modify_text
+        text = text.replace(key, num_dict[key])
+    return text
 
-#result = replace_all('Мой любимый язык программирования C++', {'Java': 'Python', 'PHP': 'Python', 'C++': 'Python'})
-#result = replace_all("one, two, three, four", {'one': 'один', 'three': 'три', 'five': 'пять', 'two': 'два'})
 result = replace_all('My favorite animal is {animal}. My favorite color is {color}.', {"{animal}":"dog", "{color}":"blue"})
-
 print(result)
+#=====================================================
+# def replace_all(text: str, data: dict):
+#     for k, v in data.items():
+#         text = text.replace(k, v)
+#     return text
+
