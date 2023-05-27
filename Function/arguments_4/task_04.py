54a7a9a2809afb1f8c583453b458ba85f85109dd
Katya <Katya11091@outlook.com>
2023-05-27 09:18:15 +0300

    add example parameters MonoBlock

--- Function/arguments_4/task_04.py
+++ Function/arguments_4/task_04.py
@@ -6,6 +6,16 @@ def sm(*num: int):
         for n in num:
             result += n
     return result
-print(sm())
+# print(sm(2, 59, 3, 90))
 
+def compare_str(s1, s2, reg=False, trim=True):
+    if reg:
+        s1 = s1.lower()
+        s2 = s2.lower()
+    if trim:
+        s1 = s1.strip()
+        s2 = s2.strip()
 
+    return s1 == s2
+
+print(compare_str("Python", "PYTHON", True, False))
