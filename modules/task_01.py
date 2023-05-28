b4c901e5c1bd255fe75d88716f6728c45f312c37
Katya <Katya11091@outlook.com>
2023-05-28 16:50:08 +0300

    in proccess monoblock

--- modules/task_01.py
+++ modules/task_01.py
@@ -1,8 +1,15 @@
-import ipaddress
+from ipaddress import ip_network
 
 
 def has_access(a, b):
-    return ipaddress(a)
+    hosts_list = list(ip_network(b).hosts())
+    ip_ad = list(ip_network(a).hosts())
+    access = False
+    for host in hosts_list:
+        print(host)
+    return ip_ad[0]
+
+
 
 
 access = has_access("91.142.84.30", "91.142.84.0/27")
