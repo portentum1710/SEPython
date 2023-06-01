786e6b5e12626c6b180838f5da637be8d0317cad
Katya <Katya11091@outlook.com>
2023-06-01 04:50:04 +0300

    *args and *kwargs lesson on monoblock

--- Function/args_kwargs/args.py
+++ Function/args_kwargs/args.py
@@ -1,10 +1,22 @@
+# позиционные аргументы всегда идут в начале
+#              СПЕЦСИМВОЛЫ:
+# / - все что слева - только позиционные аргументы
+# * - все что справа - только keyword аргумент
+# **kwargs собирает все keyword аргуметы в словарь
 a, *_, c = [1, 2, 3, 4, 5]
 
-def example(a, b, c):
+def example(a, b, *, c, d):
     print(a)
     print(b)
     print(c)
+    print(d)
+
+def my_print(*args, **kwargs):
+    print(f"Got keywords: {kwargs}")
+    for arg in args:
+        print(str(arg), **kwargs)
 
 if __name__ == '__main__':
     #print(*[1, 3, 5, 0])
-    example(1, 2, 3)
+    #example(1, 2, c=True, d=False)
+    my_print(1, 2, 3, 4, 5, sep=":", end="-")
