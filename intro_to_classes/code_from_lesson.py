8e1b203d1e05c4c2ad31adcdb57546e32597ad7d
Katya <Katya11091@outlook.com>
2023-06-02 04:34:38 +0300

    intro to Classes

---
+++ intro_to_classes/code_from_lesson.py
@@ -0,0 +1,14 @@
+class Auto:
+    def __init__(self):
+        self.number = ""
+        print("Создание обьекта")
+
+auto1 = Auto()
+auto2 = Auto()
+
+print("a1:", auto1.number)
+print("a2:", auto2.number)
+auto1.number = "a111aa"
+auto2.number = "a222aa"
+print("a1:", auto1.number)
+print("a2:", auto2.number)
